bfb8af74b7a4447dc4b1706e3ed18a2f9a34eee8
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-07-18 13:48:26 -0700

    Add jwtvalidation to users get route

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 const userController = require("../controllers/userController");
+const jwtValidation = require("../utils/jwtValidate");
 
 /* GET users listing. */
-router.get("/", userController.users_get);
+router.get("/", jwtValidation, userController.users_get);
 
 router.post("/", userController.users_post);
 
