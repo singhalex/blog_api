669b128ab17885fe46f41ec8aa0a5c4844a15eed
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-07-18 18:01:25 -0700

    Add passport local validation middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const authController = require("../controllers/authController");
+const passportLocalValidate = require("../utils/passportLocal");
 
-router.post("/login", authController.login);
+router.post("/login", passportLocalValidate, authController.login);
 
 module.exports = router;
